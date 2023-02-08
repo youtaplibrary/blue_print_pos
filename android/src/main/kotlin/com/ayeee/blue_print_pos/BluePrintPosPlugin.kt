981a3c2d24a6a763db14d3389b95c6d6f2fb810e
Iandi Santulus <iandisantulusn@gmail.com>
2023-02-08 13:43:13 +0700

    refactor(android): destroy webview on done

--- android/src/main/kotlin/com/ayeee/blue_print_pos/BluePrintPosPlugin.kt
+++ android/src/main/kotlin/com/ayeee/blue_print_pos/BluePrintPosPlugin.kt
@@ -1,7 +1,9 @@
 package com.ayeee.blue_print_pos
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
@@ -9,6 +11,7 @@ import android.view.WindowInsets
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.NonNull
+import androidx.annotation.RequiresApi
 import com.ayeee.blue_print_pos.extension.toBitmap
 import com.ayeee.blue_print_pos.extension.toByteArray
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -28,7 +31,6 @@ class BluePrintPosPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
     private lateinit var activity: Activity
     private lateinit var context: Context
-    private lateinit var webView: WebView
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         val viewID = "webview-view-type"
@@ -47,78 +49,119 @@ class BluePrintPosPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val textZoom: Int? = textScaleFactor?.let { (it * 100).toInt() }
 
         if (call.method == "contentToImage") {
-            webView = WebView(this.context)
-            val dWidth: Int
-            val dHeight: Int
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                val windowMetrics = activity.windowManager.currentWindowMetrics
-                val insets =
-                        windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
-                dWidth = windowMetrics.bounds.width() - insets.left - insets.right
-                dHeight = windowMetrics.bounds.height() - insets.bottom - insets.top
-            } else {
-                dWidth = this.activity.window.windowManager.defaultDisplay.width
-                dHeight = this.activity.window.windowManager.defaultDisplay.height
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+                result.error(
+                    "FAILED_CONTENT_TO_IMAGE",
+                    "Unsupported Android version",
+                    null
+                )
+                return
             }
-            Logger.log("\ndwidth : $dWidth")
-            Logger.log("\ndheight : $dHeight")
-            webView.layout(0, 0, dWidth, dHeight)
-            webView.loadDataWithBaseURL(null, content, "text/HTML", "UTF-8", null)
-            webView.setInitialScale(1)
-            webView.settings.javaScriptEnabled = true
-            webView.settings.useWideViewPort = true
-            webView.settings.javaScriptCanOpenWindowsAutomatically = true
-            webView.settings.loadWithOverviewMode = true
-            webView.settings.textZoom = textZoom ?: webView.settings.textZoom
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                Logger.log("\n=======> enabled scrolled <=========")
-                WebView.enableSlowWholeDocumentDraw()
-            }
-
-            Logger.log("\n ///////////////// webview setted /////////////////")
-
-            webView.webViewClient = object : WebViewClient() {
-                override fun onPageFinished(view: WebView, url: String) {
-                    super.onPageFinished(view, url)
-
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        Logger.log("\n ================ webview completed ==============")
-                        Logger.log("\n scroll delayed ${webView.scrollBarFadeDuration}")
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                            webView.evaluateJavascript("document.body.offsetWidth") { offsetWidth ->
-                                webView.evaluateJavascript("document.body.offsetHeight") { offsetHeight ->
-                                    Logger.log("\noffsetWidth : $offsetWidth")
-                                    Logger.log("\noffsetHeight : $offsetHeight")
-                                    if (offsetWidth != null && offsetWidth.isNotEmpty() && offsetHeight != null && offsetHeight.isNotEmpty()) {
-                                        val data = webView.toBitmap(
-                                                offsetWidth.toDouble(),
-                                                offsetHeight.toDouble(),
-                                                dWidth,
-                                        )
-                                        if (data != null) {
-                                            val bytes = data.toByteArray()
-                                            result.success(bytes)
-                                            Logger.log("\n Got snapshot")
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }, (duration ?: 0.0).toLong())
-                }
-            }
+            _contentToImage(content, textZoom, result, duration)
         } else {
             result.notImplemented()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun _contentToImage(
+        content: String,
+        textZoom: Int?,
+        result: Result,
+        duration: Double?
+    ) {
+        val webView = WebView(this.context)
+        val dWidth: Int
+        val dHeight: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = activity.windowManager.currentWindowMetrics
+            val insets =
+                windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            dWidth = windowMetrics.bounds.width() - insets.left - insets.right
+            dHeight = windowMetrics.bounds.height() - insets.bottom - insets.top
+        } else {
+            dWidth = activity.window.windowManager.defaultDisplay.width
+            dHeight = activity.window.windowManager.defaultDisplay.height
+        }
+        Logger.log("\ndwidth : $dWidth")
+        Logger.log("\ndheight : $dHeight")
+        webView.layout(0, 0, dWidth, dHeight)
+        webView.loadDataWithBaseURL(null, content, "text/HTML", "UTF-8", null)
+        webView.setInitialScale(1)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.useWideViewPort = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.textZoom = textZoom ?: webView.settings.textZoom
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Logger.log("\n=======> enabled scrolled <=========")
+            WebView.enableSlowWholeDocumentDraw()
+        }
+
+        Logger.log("\n ///////////////// webview setted /////////////////")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+
+                fun destroyWebView() {
+                    webView.destroy()
+                }
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    Logger.log("\n ================ webview completed ==============")
+                    Logger.log("\n scroll delayed ${webView.scrollBarFadeDuration}")
+
+                    webView.evaluateJavascript("document.body.offsetWidth") { offsetWidth ->
+                        webView.evaluateJavascript("document.body.offsetHeight") { offsetHeight ->
+                            Logger.log("\noffsetWidth : $offsetWidth")
+                            Logger.log("\noffsetHeight : $offsetHeight")
+                            if (offsetWidth == null || offsetWidth.isEmpty() || offsetHeight == null || offsetHeight.isEmpty()) {
+                                result.error(
+                                    "FAILED_CONTENT_TO_IMAGE",
+                                    "Failed to convert content to image",
+                                    null
+                                )
+                                destroyWebView()
+                                return@evaluateJavascript
+                            }
+
+                            val data: Bitmap? = webView.toBitmap(
+                                offsetWidth.toDouble(),
+                                offsetHeight.toDouble(),
+                                dWidth,
+                            )
+                            if (data == null) {
+                                result.error(
+                                    "FAILED_CONTENT_TO_IMAGE",
+                                    "Failed to convert content to image",
+                                    null
+                                )
+                            } else {
+                                val bytes: ByteArray = data.toByteArray()
+                                result.success(bytes)
+                                Logger.log("\n Got snapshot")
+                            }
+
+                            destroyWebView()
+                        }
+                    }
+
+                }, (duration ?: 0.0).toLong())
+
+            }
         }
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         Logger.log("onAttachedToActivity")
         activity = binding.activity
-        webView = WebView(activity.applicationContext)
-        webView.minimumHeight = 1
-        webView.minimumWidth = 1
+        context = activity.applicationContext
+        WebView(activity.applicationContext).apply {
+            minimumHeight = 1
+            minimumWidth = 1
+        }
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
